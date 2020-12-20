1e22ae3ce35be2cb2f8f9374c762a660c06bdbf7
cewfowler <cewfowler@gmail.com>
2020-12-20 08:01:23 -0600

    Improved efficiency

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -5,6 +5,6 @@ function J = computeCost(X, y, theta)
 
 m = length(y); % number of training examples
 
-J = 1/(2*m) * sum( ((X*theta) - y).^2 ); % Cost function
+J = 1/(2*m) * (X*theta - y)' * (X*theta - y); % Cost function
 
 end
